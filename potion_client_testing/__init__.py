4f06f00b929dee8f68c71ddeff13daff5644d552
Joao Cardoso <joaca@biosustain.dtu.dk>
2015-01-27 13:19:19 +0100

    Setup test enviroment

--- potion_client_testing/__init__.py
+++ potion_client_testing/__init__.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from http.client import parse_headers
 import json
-from io import StringIO
 from flask import Flask
 from flask_potion.routes import ItemAttributeRoute
 from flask.testing import FlaskClient
