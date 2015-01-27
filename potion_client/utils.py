0bc00889624aab266b0ebdb583c65c941e763174
Joao Cardoso <joaca@biosustain.dtu.dk>
2015-01-27 16:37:10 +0100

    Remove non-used code

--- potion_client/utils.py
+++ potion_client/utils.py
@@ -55,44 +55,6 @@ def params_to_dictionary(params_string):
     return d
 
 
-def _expand_pair(key, value, out_params):
-    if isinstance(value, dict):
-        for k, v in value.items():
-            out_params["%s[%s]" % (key, k)] = v
-    elif isinstance(value, list):
-        for i, e in enumerate(value):
-            out_params["%s[%s]" % (key, i)] = e
-    elif isinstance(value, str):
-        out_params[key] = "'%s'" % value
-    else:
-        out_params[key] = value
-
-
-def expand_params(in_params):
-    out_params = {}
-
-    if isinstance(in_params, dict):
-        for key, value in in_params.items():
-            _expand_pair(key, value, out_params)
-
-    elif isinstance(in_params, list):
-        for index, value in enumerate(in_params):
-            _expand_pair(index, value, out_params)
-
-    d = depth(out_params)
-    while d > 1:
-        out_params = expand_params(out_params)
-        d = depth(out_params)
-
-    return out_params
-
-
-def depth(dictionary, d=0):
-    if not isinstance(dictionary, dict) or not dictionary:
-        return d
-    return max([depth(v, d+1) for k, v in dictionary.items()])
-
-
 def dictionary_to_params(d):
     s = []
     for key, value in d.items():
@@ -123,14 +85,6 @@ def parse_uri(uri):
     split = uri.split("/")
     resource_name, resource_id = split[-2], split[-1]
     return resource_name, resource_id
-
-
-def evaluate_list(a_list, client):
-    if len(a_list) > 0:
-        if isinstance(a_list[0], dict) and REF in a_list[0]:
-            return [evaluate_ref(el[REF], client) for el in a_list]
-
-    return a_list
 
 
 def evaluate_ref(uri, client, instance=None):
