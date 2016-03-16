e553e329289cbec1d1188bf8ac24be3aef85b0f2
Lars Schöning <lars@lyschoening.de>
2016-03-16 15:39:49 +0100

    chore: release v2.0.0, switch to semantic versioning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Potion-client',
-    version='0.2.0',
+    version='2.0.0',
     packages=['potion_client'],
     url='https://github.com/biosustain/potion-client',
     license='MIT',
