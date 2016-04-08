04358351bbded3e66120c1c28e033aeccabce83c
Lars Schöning <lars@lyschoening.de>
2016-04-08 13:31:02 +0200

    chore: release version 2.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Potion-client',
-    version='2.0.0',
+    version='2.1.0',
     packages=['potion_client'],
     url='https://github.com/biosustain/potion-client',
     license='MIT',
